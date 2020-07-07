e2211ea769d59e23f5af5dae966d9ecb95b0a3d9
dmitriy-akkerman <dmitriy.akkerman.odesseo@gmail.com>
2020-07-07 11:06:38 +0300

    Тесты

---
+++ tests/index.test.js
@@ -0,0 +1,66 @@
+const ArmUI = require('../src/js/arm-ui');
+
+describe('ArmUI testing', () => {
+
+  test('Defining ArmUI', () => {
+    expect(ArmUI).toBeDefined();
+  });
+
+  test('Defining ArmUI.Accordion', () => {
+    expect(ArmUI.Accordion).toBeDefined();
+  });
+
+  test('Defining ArmUI.Tabs', () => {
+    expect(ArmUI.Tabs).toBeDefined();
+  });
+
+  test('Defining ArmUI.Popup', () => {
+    expect(ArmUI.Popup).toBeDefined();
+  });
+
+});
+
+describe('Popup testing', () => {
+
+  document.body.innerHTML = `
+    <div class="popup">
+      <div>Popup text</div>
+    </div>
+    <a href="#" class="j-popup">Show popup</a>
+  `;
+
+  let popup = new ArmUI.Popup({
+    el: document.querySelector('.popup'),
+    openers:  document.querySelectorAll('.j-popup'),
+    closable: true
+  });
+
+  test('ArmUI.Popup should contain root selector', () => {
+    expect(popup.el).toBeDefined();
+  });
+
+  test('Popup should be initialized', () => {
+    expect(popup).toBeTruthy();
+  });
+
+  test('Popup should contain openers', () => {
+    expect(popup.openers).toBeTruthy();
+  });
+
+});
+
+describe('Tabs testing', () => {
+
+});
+
+describe('Accordion testing', () => {
+
+});
+
+describe('SelectExtended testing', () => {
+
+  test('Defining ArmUI.SelextExtended', () => {
+    expect(ArmUI.SelectExtended).toBeDefined();
+  });
+
+});
