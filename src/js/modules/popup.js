3204020c88ce8cc68eff8cd3f221a9555617a9c4
dmitriy-akkerman <dmitriy.akkerman.odesseo@gmail.com>
2020-09-10 16:42:11 +0300

    фикс

--- src/js/modules/popup.js
+++ src/js/modules/popup.js
@@ -4,7 +4,7 @@
   } else if (typeof module === 'object' && module.exports) {
     module.exports = factory();
   } else {
-    root.Dropdown = factory();
+    root.Popup = factory();
   }
 }(typeof self !== 'undefined' ? self : this, function () {
 
