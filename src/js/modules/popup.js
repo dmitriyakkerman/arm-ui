ad1dc587fbfc5e08e0d0d418ca857832a5dd356d
dmitriyakkerman <darksider22023103>
2020-10-23 14:16:33 +0300

    дефолтное значение опции openers - массив

--- src/js/modules/popup.js
+++ src/js/modules/popup.js
@@ -22,7 +22,7 @@ const globals_1 = require("../globals/globals");
             }
             this.options = options;
             this.options.el = typeof options.el === 'string' ? document.querySelector(options.el) : options.el;
-            this.options.openers = typeof options.openers === 'string' ? document.querySelectorAll(options.openers) : options.openers;
+            this.options.openers = typeof options.openers === 'string' ? document.querySelectorAll(options.openers) : [options.openers];
             this.options.closable = options.closable || false;
             this.onInit();
             this.onLoad();
